c358c057ad576965b96c5282da2aa9e2d0058fe8
wisdingo <wisdingo@gmail.com>
2012-02-14 05:16:26 +0000

    글 쓰기 기능 완성

--- Project_UI/src/kr/co/Project_UI/SecondTab_2.java
+++ Project_UI/src/kr/co/Project_UI/SecondTab_2.java
@@ -7,39 +7,55 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SecondTab_2 extends Activity {
+public class SecondTab_2 extends Activity implements OnClickListener {
+	BoardHelper boardHelper = new BoardHelper();
 	Button prevBtn;
+	Button saveBtn;
+	EditText title;
+	EditText content;
 	String [] name = { "한식", "중식", "일식", "양식", "커피", "패스트푸드" };
 	Spinner spinVw;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		View pView = LayoutInflater.from(this.getParent()).inflate(
-				R.layout.secondtab_2_layout, null);
+		View pView = LayoutInflater.from(this.getParent()).inflate(R.layout.secondtab_2_layout, null);
 		setContentView(pView);
 		
-		prevBtn = (Button) findViewById(R.id.prevBtn);
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, name);
-		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, name);
 		spinVw = (Spinner)findViewById(R.id.spinVw);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		
 		spinVw.setAdapter(adapter);
-		
 		spinVw.setSelection(0);
 
-		prevBtn.setOnClickListener(new OnClickListener() {
+		prevBtn = (Button)findViewById(R.id.prevBtn);
+		prevBtn.setOnClickListener(this);
+		saveBtn = (Button)findViewById(R.id.saveBtn);
+		saveBtn.setOnClickListener(this);
+		
+		title = (EditText)findViewById(R.id.title);
+		content = (EditText)findViewById(R.id.content);
+	}
 
-			public void onClick(View v) {
-				SecondTab parent = ((SecondTab) getParent());
-				parent.onBackPressed();
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.prevBtn :
+			SecondTab parent = ((SecondTab) getParent());
+			parent.onBackPressed();
+			break;
+		case R.id.saveBtn :
+			if(boardHelper.writePost(new BoardVO(0, "장상윤", "777777", 
+					title.getText().toString(), content.getText().toString(), 0, null)))
+				Toast.makeText(getApplicationContext(), "등록 성공", 2000).show();
+			else {
+				Toast.makeText(getApplicationContext(), "등록 실패", 2000).show();
 			}
-		});
+			finish();
+			break;
+		}
 	}
 }
