fffb503f35fa954e1e6decfeafc56c5361bb9ba9
wisdingo <wisdingo@gmail.com>
2012-02-16 14:07:12 +0000

    NoteTab 약간 수정? SettingsTab 추가

---
+++ FoodBook/src/com/footy/FoodBook/NoteTab.java
@@ -0,0 +1,14 @@
+package com.footy.FoodBook;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class NoteTab extends ListActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.notetab);
+	}
+	
+}
