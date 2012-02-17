0f90392a3aad1d5dc485d6d22846b23dab3be3b9
kjaw85@gmail.com <kjaw85@gmail.com@dcc9f69e-1d70-b6bf-9a13-097a25c011a5>
2012-02-17 00:02:52 +0000

    time over

---
+++ trunk/FoodBook/src/com/footy/Map/MyPartyOverlay.java
@@ -0,0 +1,64 @@
+/***
+ * Copyright (c) 2010 readyState Software Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.footy.Map;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.Toast;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+
+
+public class MyPartyOverlay extends BalloonItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> m_overlays = new ArrayList<OverlayItem>();
+	private Context c;
+	
+	public MyPartyOverlay(Drawable defaultMarker, MapView mapView) {
+		super(boundCenter(defaultMarker), mapView);
+		c = mapView.getContext();
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+	    m_overlays.add(overlay);
+	    populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return m_overlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return m_overlays.size();
+	}
+
+	@Override
+	protected boolean onBalloonTap(int index) {
+		MapInfo mapInfo = MapInfo.getMapInfo();
+		mapInfo.setAddress(m_overlays.get(index).getTitle());
+		mapInfo.setPoint(m_overlays.get(index).getPoint());
+		
+		Toast.makeText(c, "ÆÄÆ¼", 2000).show();
+		return true;
+	}
+	
+}
