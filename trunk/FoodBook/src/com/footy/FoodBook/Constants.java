040c83c51a4a4a783328c86f22bcd5f9730b92d5
wisdingo@gmail.com <wisdingo@gmail.com@dcc9f69e-1d70-b6bf-9a13-097a25c011a5>
2012-02-17 03:12:15 +0000

    발표 ㅠㅠ 어떻게함

--- trunk/FoodBook/src/com/footy/FoodBook/Constants.java
+++ trunk/FoodBook/src/com/footy/FoodBook/Constants.java
@@ -1,7 +1,7 @@
 package com.footy.FoodBook;
 
 public class Constants {
-	public static final String WEB_SERVER_URL = "http://121.169.38.155:8000/android-3/";
+	public static final String WEB_SERVER_URL = "http://115.145.172.123:8000/android-3/";
 	public static boolean FILTER_BOARD = true;
 	public static boolean FILTER_FOODPARTY = true;
 	
